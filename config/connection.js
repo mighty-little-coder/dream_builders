8741f81b9381230b71a5fc063a89dafa5a3c7755
Chris Rice <chrisrice2442@yahoo.com>
2024-01-06 19:44:52 -0500

    connection.js should be good to go

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: process.env.HOST,
+      dialect: 'mysql',
+      port: 3301
+      },
+    );
+
+module.exports = sequelize;
