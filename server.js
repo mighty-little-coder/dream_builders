cdb704b373022dae5ef39cb08265c7b09d7eb0be
LewisSin <137732215+LewisSin@users.noreply.github.com>
2024-01-09 19:28:37 -0500

    I GOT JS TO WORK

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (you may want to restrict this to your domain)
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
